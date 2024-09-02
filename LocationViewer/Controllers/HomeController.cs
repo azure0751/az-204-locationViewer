04a21445f272fd5b3e0187cd242fde0149bebea3
Azure0751 <58366593+abhi.0751@users.noreply.github.com>
2024-09-02 17:58:46 +0530

    location viewer code added

---
+++ LocationViewer/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+using LocationViewer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace LocationViewer.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            var regionName = Environment.GetEnvironmentVariable("REGION_NAME") ?? "Unknown Location";
+            ViewData["Location"] = regionName;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
